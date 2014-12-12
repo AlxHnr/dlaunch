6fe45bba7f59bac5d2278050ce2658ec0b73d610
Alexander Heinrich <alxhnr@nudelpost.de>
2014-12-12 21:06:38 +0100

    Added plugin API, loader and un/install targets

--- Makefile
+++ Makefile
@@ -1,5 +1,29 @@
 CSC_FLAGS   = -O3
+INSTALL_PREFIX ?= /usr/local
+PLUGIN_API_PATH = $(INSTALL_PREFIX)/share/dlaunch/plugin-api
 
 -include build/extra.makefile
 build/extra.makefile:
 	csi -s chicken-builder/generate-extra-makefile.scm
+
+all: build/dlaunch-plugin-api.import.so
+build/dlaunch-plugin-api.import.so: \
+  src/dlaunch-plugin-api.scm | build/dlaunch.o
+	cd build/ && \
+	  csc $(CSC_FLAGS) -J -c ../$< -o ../$(@:%.import.so=%.o) && \
+	  csc $(CSC_FLAGS) -dynamic ../$(@:%.so=%.scm) -o ../$@ && \
+	  rm ../$(@:%.import.so=%.o) ../$(@:%.so=%.scm)
+
+.PHONY: install uninstall
+install: all
+	mkdir -p "$(INSTALL_PREFIX)/bin/"
+	mkdir -p "$(PLUGIN_API_PATH)"
+	cp build/dlaunch "$(INSTALL_PREFIX)/bin/"
+	cp build/dlaunch-plugin-api.import.so "$(PLUGIN_API_PATH)"
+
+uninstall:
+	rm "$(INSTALL_PREFIX)/bin/dlaunch"
+	rm "$(PLUGIN_API_PATH)/dlaunch-plugin-api.import.so"
+	rmdir "$(PLUGIN_API_PATH)/"
+	rmdir --ignore-fail-on-non-empty "$(INSTALL_PREFIX)/bin/"
+	rmdir --ignore-fail-on-non-empty "$(INSTALL_PREFIX)/share/"
---
+++ src/plugin-loader.scm
@@ -0,0 +1,107 @@
+; Copyright (c) 2014 Alexander Heinrich <alxhnr@nudelpost.de>
+;
+; This software is provided 'as-is', without any express or implied
+; warranty. In no event will the authors be held liable for any damages
+; arising from the use of this software.
+;
+; Permission is granted to anyone to use this software for any purpose,
+; including commercial applications, and to alter it and redistribute it
+; freely, subject to the following restrictions:
+;
+;    1. The origin of this software must not be misrepresented; you must
+;       not claim that you wrote the original software. If you use this
+;       software in a product, an acknowledgment in the product
+;       documentation would be appreciated but is not required.
+;
+;    2. Altered source versions must be plainly marked as such, and must
+;       not be misrepresented as being the original software.
+;
+;    3. This notice may not be removed or altered from any source
+;       distribution.
+
+(chb-module plugin-loader (compile-changed-plugins load-plugins)
+  (chb-import base-directories)
+  (use utils files extras posix srfi-1 srfi-13 srfi-69)
+
+  ;; A hash table, which contains the names of all loaded plugins.
+  (define loaded-plugins (make-hash-table))
+
+  ;; The full path to the file containing compilation informations.
+  (define build-data-file (get-cache-path "compiled-plugins.scm"))
+
+  ;; A hash table, which associates plugin names with the last modification
+  ;; time of the corresponding plugin.
+  (define build-data
+    (if (file-exists? build-data-file)
+      (alist->hash-table (car (read-file build-data-file)))
+      (make-hash-table)))
+
+  ;; The directory path to the plugin-api import libraries.
+  (define-constant plugin-api-import-path
+    (string-append
+      (or (get-environment-variable "INSTALL_PREFIX") "/usr/local")
+      "/share/dlaunch/plugin-api/"))
+
+  ;; Calls the given procedure for each plugin with the current working
+  ;; directory set to the plugin-api import path. The given procedure takes
+  ;; three arguments: the plugin name, the full path to its source file and
+  ;; the full path to its output file.
+  (define (for-each-plugin-with-chdir proc)
+    (let ((pwd (current-directory)))
+      (change-directory plugin-api-import-path)
+      (for-each
+        (lambda (filename)
+          (proc
+            (pathname-file filename)
+            (get-config-path "plugins/" filename)
+            (get-cache-path "plugins/" (pathname-file filename) ".so")))
+        (if (directory-exists? (get-config-path "plugins/"))
+          (filter
+            (lambda (filename) (string-suffix-ci? ".scm" filename))
+            (directory (get-config-path "plugins/")))
+          '()))
+      (change-directory pwd)))
+
+  ;; Compiles a scheme file for dynamic loading.
+  (define (compile-scheme-file plugin-name source-file output-file)
+    (compile-file
+      source-file
+      #:options (list "-O3" "-dynamic" "-unit" plugin-name)
+      #:output-file output-file #:load #f))
+
+  ;; Checks if the source file for the given plugin has changed and
+  ;; rebuilds the corresponding dynamic library. If the dynamic library
+  ;; does not not exist it will be created. The path to the output file
+  ;; must exist.
+  (define (update-compiled-plugin plugin-name source-file output-file)
+    (define curr-time (vector-ref (file-stat source-file) 8))
+    (define prev-time (hash-table-ref/default build-data plugin-name 0))
+    (unless (and (file-exists? output-file) (= prev-time curr-time))
+      (if (compile-scheme-file plugin-name source-file output-file)
+        (begin
+          (print "compiled plugin '" plugin-name "'.")
+          (hash-table-set! build-data plugin-name curr-time))
+        (error
+          (string-append
+            "failed to compile the plugin '" plugin-name "'.")))))
+
+  ;; Compiles all plugins, which either don't exist or have been modified
+  ;; since their last compilation.
+  (define (compile-changed-plugins)
+    (create-directory (get-cache-path "plugins/") #t)
+    (for-each-plugin-with-chdir update-compiled-plugin)
+    (call-with-output-file
+      build-data-file
+      (lambda (out)
+        (write (hash-table->alist build-data) out))))
+
+  ;; Loads a plugin if it was not loaded already.
+  (define (load-plugin plugin-name source-file output-file)
+    (unless (hash-table-exists? loaded-plugins plugin-name)
+      (load-library (string->symbol plugin-name) output-file)
+      (hash-table-set! loaded-plugins plugin-name #t)
+      (print "loaded '" output-file "'.")))
+
+  ;; Loads all plugins, which where not loaded already.
+  (define (load-plugins)
+    (for-each-plugin-with-chdir load-plugin)))
