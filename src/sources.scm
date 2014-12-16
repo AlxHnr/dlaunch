cbd6c8a80b4b6bf3583d93a1daec4f018281d49b
Alexander Heinrich <alxhnr@nudelpost.de>
2014-12-16 23:30:22 +0100

    Filter out non-existing items which where scored

--- src/sources.scm
+++ src/sources.scm
@@ -33,59 +33,37 @@
   ;;       cached by a background process for subsequent runs.
   (define-record source-info thunk async)
 
-  ;; If the given source list is empty, it will return a list with all
-  ;; sources available.
-  (define (fallback-source-list source-list)
-    (if (null? source-list)
-      (map car (hash-table->alist source-table))
-      source-list))
-
   ;; Registers a source and errors if it is already registered. It takes
   ;; optional key values as described above in 'source-info'.
-  (define (register-source name thunk #!key async)
+  (define (register-source source-name thunk #!key async)
     (cond
-      ((substring-index name ">  ")
-       (die "source name cannot contain the '>  ' separator: '" name "'."))
-      ((hash-table-exists? source-table name)
-       (die
-         "multiple sources with the same name are forbidden: '" name "'."))
+      ((substring-index source-name ">  ")
+       (die "source name cannot contain the '>  ' separator: '"
+            source-name "'."))
+      ((hash-table-exists? source-table source-name)
+       (die "multiple sources with the same name are forbidden: '"
+            source-name "'."))
       (else
         (hash-table-set!
-          source-table name
+          source-table source-name
           (make-source-info thunk async)))))
 
   ;; Checks if a source was registered.
-  (define (source-exists? name)
-    (hash-table-exists? source-table name))
+  (define (source-exists? source-name)
+    (hash-table-exists? source-table source-name))
 
-  ;; Update the cache, if it is not being updated already by another
-  ;; process. It takes the name of the source and its source function as
-  ;; arguments. If 'wait' is true, it will wait for running background
-  ;; processes to terminate.
-  (define (synced-cache-update name source-function wait)
-    (create-directory (get-cache-path ".async/" name) #t)
-    (define lock-port
-      (open-output-file (get-cache-path ".async/" name "/lockfile")))
-    (define lock
-      (condition-case
-        (file-lock lock-port)
-        ((exn file)
-         (if wait
-           (file-unlock (file-lock/blocking lock-port)))
-         (close-output-port lock-port)
-         #f)))
-    (when lock
-      (call-with-output-file
-        (get-cache-path ".async/" name "/.cache")
-        (lambda (out)
-          (for-each
-            (lambda (item)
-              (write-line item out))
-            (reverse (source-function)))))
-      (file-move
-        (get-cache-path ".async/" name "/.cache")
-        (get-cache-path ".async/" name "/cache") #t)
-      (close-output-port lock-port)))
+  ;; Deduplicates the given source list. If the given list is null, it will
+  ;; return a list with all registered sources.
+  (define (prepare-source-list source-list)
+    (if (null? source-list)
+      (map car (hash-table->alist source-table))
+      (let ((sorted-list (sort source-list string<?)))
+        (fold
+          (lambda (source-name lst)
+            (if (string=? source-name (car lst))
+              lst (cons source-name lst)))
+          (list (car sorted-list))
+          (cdr sorted-list)))))
 
   ;; Read lines in reversed order. This is slightly faster than
   ;; 'read-lines' from extras.
@@ -96,90 +74,112 @@
           cons '()
           (lambda () (read-line in))))))
 
+  ;; Update the cache, if it is not being updated already by another
+  ;; process. It takes the name of the source and its source function as
+  ;; arguments. If 'wait' is true, it will wait for running background
+  ;; processes to terminate.
+  (define (synced-cache-update source-name source-function wait)
+    (create-directory (get-cache-path ".async/" source-name) #t)
+    (define lock-port
+      (open-output-file
+        (get-cache-path ".async/" source-name "/lockfile")))
+    (define lock
+      (condition-case
+        (file-lock lock-port)
+        ((exn file)
+         (if wait
+           (file-unlock (file-lock/blocking lock-port)))
+         (close-output-port lock-port)
+         #f)))
+    (when lock
+      (call-with-output-file
+        (get-cache-path ".async/" source-name "/.cache")
+        (lambda (out)
+          (for-each
+            (lambda (item)
+              (write-line item out))
+            (reverse (source-function)))))
+      (file-move
+        (get-cache-path ".async/" source-name "/.cache")
+        (get-cache-path ".async/" source-name "/cache") #t)
+      (close-output-port lock-port)))
+
   ;; Returns the raw contents of a source. If the 'async' flag is set for
   ;; this source, it will return its cache. If no cache is available, it
   ;; will create one and block until its done.
-  (define (collect-source-contents name info)
+  (define (collect-source-contents source-name info)
     (if (source-info-async info)
-      (let ((cache-file (get-cache-path ".async/" name "/cache")))
+      (let ((cache-file (get-cache-path ".async/" source-name "/cache")))
         (if (file-exists? cache-file)
           (let ((cache-data (read-lines-reversed cache-file)))
             (process-fork
               (lambda ()
                 (synced-cache-update
-                  name (source-info-thunk info) #f)))
+                  source-name (source-info-thunk info) #f)))
             cache-data)
           (begin
-            (print "initializing cache for the source '" name "' ...")
-            (synced-cache-update name (source-info-thunk info) #t)
+            (print
+              "initializing cache for the source '" source-name "' ...")
+            (synced-cache-update source-name (source-info-thunk info) #t)
             (read-lines cache-file))))
       ((source-info-thunk info))))
 
-  ;; A wrapper around 'collect-source-contents', which filters out all
-  ;; items with a score.
-  (define (collect-ranked-source-contents name info)
-    (let ((source-content (collect-source-contents name info))
-          (score-alist (get-score-alist name)))
+  ;; Pair each item in the given list with the given source name.
+  (define (attach-source-name lst source-name)
+    (map (lambda (item) (cons item source-name)) lst))
+
+  ;; Collects all items from a source and returns two values. The first
+  ;; value is a list of unscored pairs, associating items with their
+  ;; sources. The second returned value is a hash table, containing scored
+  ;; items which do not exist in the source at all. If this table is empty,
+  ;; the second returned value will be #f.
+  (define (get-filtered-source-content source-name info)
+    (let
+      ((source-content (collect-source-contents source-name info))
+       (score-alist (get-score-alist source-name)))
       (if score-alist
-        (let ((filter-table (alist->hash-table score-alist)))
-          (filter
-            (lambda (item)
-              (cond
-                ((zero? (hash-table-size filter-table)) #t)
-                ((hash-table-exists? filter-table item)
-                 (hash-table-delete! filter-table item)
-                 #f)
-                (else #t)))
-            source-content))
-        source-content)))
+        (let*
+          ((filter-table (alist->hash-table score-alist))
+           (unscored-items
+             (filter
+               (lambda (item)
+                 (cond
+                   ((zero? (hash-table-size filter-table)) #t)
+                   ((hash-table-exists? filter-table item)
+                    (hash-table-delete! filter-table item)
+                    #f)
+                   (else #t)))
+               source-content)))
+          (values
+            (attach-source-name unscored-items source-name)
+            (if (zero? (hash-table-size filter-table))
+              #f filter-table)))
+        (values (attach-source-name source-content source-name) #f))))
 
-  ;; Returns the content of a source as an alist, which associates each
-  ;; item with its source name. The returned alist does not contain ranked
-  ;; items.
-  (define (gather-source name)
-    (define info (hash-table-ref source-table name))
-    (map
-      (lambda (item) (cons item name))
-      (collect-ranked-source-contents name info)))
+  ;; Returns all existing, and scored items from a source in form of a
+  ;; pair list which may be null. The car of each pair contains its score.
+  ;; The pairs cdr is another pair, containing a item and its source name.
+  ;; 'ignore-table' can either be a hash table containing items to ignore,
+  ;; or #f.
+  (define (get-existing-score-infos source-name ignore-table)
+    (let ((score-alist (get-score-alist source-name)))
+      (if score-alist
+        (map
+          (lambda (score-pair)
+            (cons
+              (cdr score-pair)
+              (cons (car score-pair) source-name)))
+          (if ignore-table
+            (remove
+              (lambda (score-pair)
+                (hash-table-exists? ignore-table (car score-pair)))
+              score-alist)
+            score-alist))
+        '())))
 
-  ;; Gathers the contents of all sources in the given source list, without
-  ;; scored items. If the list is empty, it will gather all available
-  ;; sources. If the list is not empty, it must contain only valid source
-  ;; names. Duplicates will be ignored.
-  (define (gather-from-source-list source-list)
-    (fold
-      (lambda (source-name lst)
-        (merge
-          (gather-source source-name) lst
-          (lambda (a b)
-            (< (string-length (car a))
-               (string-length (car b))))))
-      '()
-      ; Deduplicate list of sources.
-      (let
-        ((sorted-list (sort (fallback-source-list source-list) string<?)))
-        (fold
-          (lambda (name lst)
-            (if (string=? name (car lst))
-              lst
-              (cons name lst)))
-          (list (car sorted-list))
-          (cdr sorted-list)))))
-
-  ;; Builds a new alist from the given score list with the given source
-  ;; name and merges it into another. This alist associates a score with a
-  ;; pair containing a string and its source.
-  (define (merge-scored-lists score-alist source-name lst)
-    (merge
-      (map
-        (lambda (score-pair)
-          (cons
-            (cdr score-pair)
-            (cons (car score-pair) source-name)))
-        score-alist)
-      lst
-      (lambda (a b)
-        (> (car a) (car b)))))
+  (define (filter-source source-name)
+    (get-filtered-source-content
+      source-name (hash-table-ref source-table source-name)))
 
   ;; Gathers all informations from the given sources. It takes a list of
   ;; valid, existing source names as an optional argument. If it is omitted
@@ -188,13 +188,32 @@
     (let ((invalid-source (find (complement source-exists?) source-list)))
       (if invalid-source
         (die "source does not exist: '" invalid-source "'")))
-    (append
-      (map cdr
-        (fold
-          (lambda (source-name lst)
-            (let ((score-alist (get-score-alist source-name)))
-              (if score-alist
-                (merge-scored-lists score-alist source-name lst)
-                lst)))
-          '() (fallback-source-list source-list)))
-      (gather-from-source-list source-list))))
+    (let*
+      ((sorted-source-list (prepare-source-list source-list))
+       (final-list-pair
+         (fold
+           (lambda (source-name list-pair)
+             (let-values
+               (((content ignore-table) (filter-source source-name)))
+               (cons
+                 (merge
+                   (get-existing-score-infos source-name ignore-table)
+                   (car list-pair)
+                   (lambda (a b)
+                     (> (car a) (car b))))
+                 (merge
+                   content (cdr list-pair)
+                   (lambda (a b)
+                     (< (string-length (car a))
+                        (string-length (car b))))))))
+           (let*-values
+             (((content ignore-table)
+               (filter-source (car sorted-source-list))))
+             (cons
+               (get-existing-score-infos
+                 (car sorted-source-list) ignore-table)
+               content))
+           (cdr sorted-source-list))))
+      (append
+        (map cdr (car final-list-pair))
+        (cdr final-list-pair)))))
